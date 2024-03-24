019a23574c0178b7a9909792a074f4417de6aa1b
Steven Le <stevenleusa79@yahoo.com>
2024-03-24 00:45:03 -0700

    upload logic done, need to add playlist table to supabase

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,23 +1,108 @@
 "use client";
 
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import uniqid from "uniqid";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+
 import Modal from "./Modal"
 import useUploadModal from "@/hooks/useUploadModal";
+import Input from "./Input";
+import Button from "./Button";
+import { useUser } from "@/hooks/useUser";
 
 const UploadModal = () => {
   const { onClose, isOpen } = useUploadModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      title: '',
+      image: null,
+    }
+  }
+  );
 
   const onChange = (open: boolean) => {
     if (!open) {
       // reset the form
-
+      reset();
       // close the modal
       onClose();
     }
   }
 
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // upload to supabase
+    try {
+      setIsLoading(true);
+
+      const imageFile = values.image?.[0];
+      
+      if (!imageFile || !user) {
+        toast.error("Missing fields.");
+        return;
+      }
+
+      const uniqueId = uniqid();
+
+      // Upload Image
+      const {
+        data: imageData,
+        error: imageError,
+      } = await supabaseClient.storage.from('images').upload(`images-${values.title}-${uniqueId}`, imageFile, {
+        cacheControl: '3600',
+        upsert: false
+      });
+
+      if (imageError) {
+        setIsLoading(false);
+        return toast.error('Failed image upload.')
+      }
+
+      // successful --> let's actually add the playlist to our databse
+      const {
+        error: supabaseError
+      } = await supabaseClient.from('playlists').insert({
+        
+      })
+
+      if (supabaseError) {
+        setIsLoading(false);
+        return toast.error(supabaseError.message);
+      }
+
+      router.refresh();
+      setIsLoading(false);
+      toast.success("Playlist created!")
+      reset();
+      onClose();
+    } catch (error) {
+      toast.error("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Modal title="Generate a new playlist!" description="🎵 Upload a photo and turn memories into music 🎵" isOpen={isOpen} onChange={onChange}>
-      Upload Content
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+        <Input id='title' disabled={isLoading} {...register('title', { required: true })} placeholder="Playlist Title"/>
+        <Input id='description' disabled={isLoading} {...register('description', { required: false })} placeholder="Description (Optional)"/>
+        <div>
+            <div className="pb-1">
+              Upload an image!
+            </div>
+            <Input id='image' type='file' disabled={isLoading} {...register('image', { required: true })} accept="image/*" className="hover:cursor-pointer"/>
+        </div>
+        <Button disabled={isLoading} type='submit'>
+          Create
+        </Button>
+      </form>
     </Modal>
   )
 }
