13629f5ec5c7b36367cbb268f3861b1553abf13d
Steven Le <stevenleusa79@yahoo.com>
2024-04-02 01:40:13 -0700

    removed account page button

--- components/Header.tsx
+++ components/Header.tsx
@@ -64,7 +64,7 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
               <Button onClick={handleLogout} className="bg-white px-6 py-2">
                 Logout
               </Button>
-              <Button onClick={() => router.push('/account')} className="bg-white">
+              <Button className="bg-white">
                 <FaUserAlt/>
               </Button>
             </div>
