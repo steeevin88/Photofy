a6206d1a63beda8c55d7395e999a9d03c6619944
Steven Le <stevenleusa79@yahoo.com>
2024-04-02 01:06:30 -0700

    added link/button to playlist on Spotify, fixed playlist description wrapping

--- app/playlists/[playlist_id]/PlaylistPage.tsx
+++ app/playlists/[playlist_id]/PlaylistPage.tsx
@@ -6,6 +6,8 @@ import Header from "@/components/Header";
 import { Playlist, SpotifyTrack } from '@/types';
 import useLoadImage from "@/hooks/useLoadImage";
 import PlaylistSong from './components/PlaylistSong';
+import Button from '@/components/Button';
+import Link from 'next/link';
 
 interface PlaylistPageProps {
   playlistInfo: Playlist;
@@ -15,19 +17,24 @@ interface PlaylistPageProps {
 const PlaylistPage: React.FC<PlaylistPageProps> = ({playlistInfo, songs}) => {
   const imageUrl = useLoadImage(playlistInfo);
   return (
-    <Header className="rounded-lg  bg-gradient-to-b from-emerald-800 to-black h-full">
+    <Header className="rounded-lg bg-gradient-to-b from-emerald-800 to-black h-full">
       {/** Playlist Details */}
-      <div className="mt-20">
+      <div className="mt-20 flex lg:flex-row justify-center lg:justify-between mx-10">
         <div className="flex flex-col md:flex-row items-center gap-x-5">
-          <div className="relative h-32 w-32 lg:h-44 lg:w-44 rounded  overflow-hidden">
+          <div className="relative h-32 w-32 lg:h-44 lg:w-44 rounded overflow-hidden">
             <Image className="object-cover shadow-xl bg-white" fill src={imageUrl || "/images/liked.png"} alt="Playlist Image" />
           </div>
           <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
             <p className="hidden md:block font-semibold text-sm">Playlist</p>
             <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">{playlistInfo.title}</h1>
-            <p className="hidden md:block text-sm">{playlistInfo.description}</p>
+            <p className="hidden lg:block text-sm break-words w-[25vw]">{playlistInfo.description}</p>
           </div>
         </div>
+        <Link href={playlistInfo.playlist_url} target='_blank' rel='noopener noreferrer'>
+          <Button className="hidden lg:block w-40 h-20">
+            View on Spotify
+          </Button>
+        </Link>
       </div>
       {/** Songs */}
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-y-2 w-full p-6">
