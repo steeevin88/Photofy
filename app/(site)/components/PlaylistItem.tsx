e2b1916603a445746a39afb0052b3166e5641039
Steven Le <stevenleusa79@yahoo.com>
2024-04-03 02:12:54 -0700

    add sizes prop so next stops yelling

--- app/(site)/components/PlaylistItem.tsx
+++ app/(site)/components/PlaylistItem.tsx
@@ -15,7 +15,7 @@ const PlaylistItem: React.FC<PlaylistItemProps> = ({data}) => {
   return (
     <Link href={{pathname: `/playlists/${data.id}`}} className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3"> 
       <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
-        <Image className="object-cover bg-white" src={imagePath || '/images/liked.png'} fill alt="image"/>
+        <Image className="object-cover bg-white" src={imagePath || '/images/liked.png'} fill alt="image" sizes="(max-width: 600px) 100vw, 800px"/>
       </div>
       <div className="flex flex-col items-start w-full pt-4 gap-y-1">
         <p className="font-semibold truncate w-full">
